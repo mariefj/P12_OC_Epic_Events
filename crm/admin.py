8630db4584ae502f815e60dd4ce4ae36bf87e867
mariefj <marie.forja@gmail.com>
2023-02-20 18:45:57 +0100

    change formfield admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 
+from authentication.models import User
 from .models import Client, Contract, Event
 
 admin.site.disable_action('delete_selected')
 
-admin.site.register(Client)
-admin.site.register(Contract)
-admin.site.register(Event)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'sales_contact':
+            kwargs["queryset"] = User.objects.filter(role="Sales")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    model = Contract
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'sales_contact':
+            kwargs["queryset"] = User.objects.filter(role="Sales")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'support_contact':
+            kwargs["queryset"] = User.objects.filter(role="Support")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 
